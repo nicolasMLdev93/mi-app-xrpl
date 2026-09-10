2e5183bf43a4c3b011d07584fb5d45c77f8c512f
Nicolas Bauza <nicolasbauza21@gmail.com>
2026-09-09 23:51:29 -0300

    Se agregó .env.example para poder implementar proyecto con bases de datos personales.

---
+++ capa_grafica/src/utils/config.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'http://localhost:3000/api';
+export const RLUSD_CURRENCY = '524C555344000000000000000000000000000000';
+export const RLUSD_ISSUER = 'rQhWct2fv4Vc4KRjRgMrxa8xPN9Zx9iLKV';
+export const XRPL_TESTNET = 'wss://s.altnet.rippletest.net:51233';
+export const XRPL_DEVNET = 'wss://s.devnet.rippletest.net:51233';
